fe0b7eed41b3d80f4c85fd839697c5e9a5ce1f66
DarkHost <sultanisk@mail.ru>
2024-03-03 00:50:43 +0500

    Update findLyricsRoute.js

--- src/routes/findLyricsRoute.js
+++ src/routes/findLyricsRoute.js
@@ -11,17 +11,18 @@ router.get('/lyrics', requireAuth, async (req, res) => {
 
 router.post('/lyrics', async (req, res) => {
     try {
+        const preferredLanguage = req.session.language || 'english';
         const { artist, track } = req.body;
         if (!artist || !track) {
             return res.status(400).send('Artist and track name are required.');
         }
 
         // lyrics.ovh API
-        const url = `https://api.lyrics.ovh/v1/${encodeURIComponent(artist)}/${encodeURIComponent(track)}`;
+        const url = `https://api.lyrics.ovh/v1/${artist}/${track}`;
         const response = await axios.get(url);
         const lyrics = response.data.lyrics;
 
-        res.render(path.join(__dirname, '..', 'public', 'pages', 'lyrics.ejs'), { artist, track, lyrics });
+        res.render(path.join(__dirname, '..', 'public', 'pages', 'lyrics.ejs'), { artist, track, lyrics, preferredLanguage });
     } catch (error) {
         console.error('Error fetching lyrics:', error);
         res.status(500).send('Error fetching lyrics.');
